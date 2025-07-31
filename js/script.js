8a833d22e52943d94d8c85c7bad04dff6a695713
myatmh <myatminhtwe7@gmail.com>
2025-07-31 12:52:26 +0630

    show categories and get data with event

--- js/script.js
+++ js/script.js
@@ -1,3 +1,6 @@
+const categoryContainer = document.querySelector(".category_container");
+
+let questionData = [];
 //Fetch Json Function
 const url = "./js/data.json";
 const fetchData = async () => {
@@ -7,7 +10,31 @@ const fetchData = async () => {
       throw new Error(`Response Status: ${response.status}`);
     }
     const data = await response.json();
-    console.log(data);
+    questionData = data;
+    console.log(questionData);
+
+    //Render Categories
+    questionData.forEach((q) => {
+      // console.log(q.category);
+
+      const categoryText = q.category;
+
+      const category = document.createElement("div");
+      category.classList.add("category");
+      category.textContent = categoryText;
+
+      categoryContainer.append(category);
+
+      category.addEventListener("click", (e) => {
+        const catTextNode = e.target.textContent;
+        const result = questionData.find((cat) => cat.category === catTextNode);
+        console.log(result);
+      });
+    });
+
+    //Get Quantity
+    const quantityTag = new Array(...document.querySelectorAll(".quantity"));
+    console.log(quantityTag);
   } catch (error) {
     console.error("Error: ", error);
   }
